9f285b92672d912d94a1876eaef6f2ee6cf90b12
madortill <mador.till@gmail.com>
2025-03-31 18:18:07 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <opening-page @start="start"></opening-page>
+  </div>
+   
+
+</template>
+
+<script>
+import OpeningPage from "@/components/OpeningPage.vue";
+export default {
+  name: "app",
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    OpeningPage
+  },
+}
+</script>
+
+<style scoped>
+#app {
+  background-size: 100vw 100vh;
+  height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  direction: rtl;
+  overflow: hidden;
+  background-image: url("./assets/media/Artboard 1.svg");
+}
+
+
+
+</style>
---
+++ src/components/OpeningPage.vue
@@ -0,0 +1,73 @@
+<template>
+    <div id="opening-page">
+        <p v-if="!isStart" class="title">לומדת פיצוץ אסטרואידים</p>
+        <img v-if="!isStart" src="@/assets/media/rocket.svg" class="rocket" @click="start" alt="">
+    </div>
+</template>
+<script>
+  export default {
+    name: "app",
+    props: [],
+    data() {
+      return{
+        isStart: false,
+      };
+    },
+    methods: {
+      start() {
+        this.isStart = true;
+        this.$emit('start')
+      }
+    }
+  };
+</script>
+<style scoped>
+    html {
+	Font-size: calc(10px + 0.5vw);
+}
+
+@font-face {
+  font-family: "abraham";
+  src: url("./assets/Abraham-Regular.ttf");
+}
+.title {
+  font-family: "abraham";
+  color: white;
+  font-size: 2.9rem;
+  position: absolute;
+  text-align: center;
+  width: 20rem;
+  right: 50%;
+  top: 5rem;
+  transform: translateX(50%);
+}
+.rocket {
+  width: 15rem;
+  position: absolute;
+  right: 40%;
+  top: 15rem;
+  transform: translateX(50%);
+
+}
+@keyframes rocket{
+  0% {
+    width: 15rem;
+  }
+  50% {
+    width: 18rem;
+  }
+  100% {
+    width: 15rem;
+  }
+}
+.rocket {
+  animation: rocket 2s infinite;
+}
+@media (max-width: 280px) {
+  .title {
+    font-size: 2rem;
+    width: 15rem;
+  }
+
+}
+</style>
